f07770b2b57b0b9c6e6d63912836b1b4f78ee0d9
SimonBoothroyd <simon.boothroyd@colorado.edu>
2020-02-20 12:08:05 -0700

    Remove unused.

--- surrogates/models/models.py
+++ surrogates/models/models.py
@@ -7,6 +7,7 @@ class Model(abc.ABC):
     for a range of temperatures.
     """
 
+    @abc.abstractmethod
     def evaluate(self, parameters, temperatures):
         """Evaluate this model for a set of parameters.
 
@@ -30,32 +31,6 @@ class Model(abc.ABC):
         numpy.ndarray
             The values of the surface tension evaluated at each temperature
             and using the specified parameters (shape=(n_temperatures, 1)).
-        """
-        raise NotImplementedError()
-
-    def evaluate_gradients(self, parameters, temperatures):
-        """Evaluate the gradients of this model for a set of parameters.
-
-        Parameters
-        ----------
-        parameters: numpy.ndarray
-            The values of the parameters to evaluate at with
-            shape=(n parameters, 1).
-        temperatures: numpy.ndarray
-            The temperatures to evaluate the properties at with
-            shape=(n_temperatures).
-
-        Returns
-        -------
-        numpy.ndarray
-            The gradients of the liquid density evaluated at each temperature
-            with respect to the specified parameters (shape=(n_temperatures, n_parameters)).
-        numpy.ndarray
-            The gradients of the vapor pressure evaluated at each temperature
-            with respect to the specified parameters (shape=(n_temperatures, n_parameters)).
-        numpy.ndarray
-            The gradients of the surface tension evaluated at each temperature
-            with respect to the specified parameters (shape=(n_temperatures, n_parameters)).
         """
         raise NotImplementedError()
 
